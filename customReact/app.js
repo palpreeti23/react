133fda5b8c773bc7abbdf9854e372d891f0439ee
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-08-31 15:30:56 +0000

    feat: include demo for React code evaluation

---
+++ customReact/app.js
@@ -0,0 +1,35 @@
+function customRender(renderElement, container){
+
+    // let domRender = document.createElement(renderElement.type);
+
+    // domRender.innerHTML = renderElement.Children;
+
+    // domRender.setAttribute('href', renderElement.props.href);
+    // domRender.setAttribute('target', renderElement.props.target);
+
+    // container.appendChild(domRender);
+
+    let domRender = document.createElement(renderElement.type);
+    domRender.innerHTML = renderElement.Children;
+    for (const prop in renderElement.props) {
+        if(prop === 'children')continue;
+       domRender.setAttribute(prop, renderElement.props[prop])
+        }
+        container.appendChild(domRender)
+    }
+
+
+
+
+const renderElement ={
+    type : "a",
+    props:{
+        href : "https://www.google.com/",
+        target : "blank"
+    },
+
+    Children : "click here to open google"
+}
+const mainContainer = document.querySelector('#root');
+
+customRender(renderElement, mainContainer);
