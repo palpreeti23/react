f4efb480196ce8766ff3a0cad3769c60d44b14cb
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-09-09 18:30:12 +0000

    done

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,46 +1,97 @@
-import { useState } from 'react'
+
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [character, setCharacter] = useState(false);
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    if (number) str += '1234567890';
+    if (character) str += '~!@#$%^&*(){}[]+_=?/|'
+
+    for (let index = 1; index <= length; index++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+  }, [length, number, character, setPassword])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, number, character, passwordGenerator])
+
+
+
+  const copyFromClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 101)
+    window.navigator.clipboard.writeText(password)
+  }, [password]
+  )
+
 
   return (
     <>
-      <div className='w-full h-screen'>
-        <div className='fixed mt-20 flex place-content-center text-white inset-x-0'>
-          <div className='w-[50%] h-auto bg-gray-700 px-3 py-2 rounded-lg'>
-            <h2 className='text-center text-2xl font-semibold mb-2'>Password Generator</h2>
-            <input
-              type="text"
-              placeholder='Password....'
-              className='w-[85%] h-10 bg-gray-300 text-black rounded-l-lg px-6 text-lg ml-2'
-            />
-            <button 
-            className='bg-blue-700 px-4 h-10 rounded-r-lg text-lg font-medium text-center focus:outline-2 focus:outline-orange-500 focus:bg-blue-800 hover:bg-blue-500'
-            >copy</button>
-            <div className='flex mt-5 mb-2 mx-1 '>
-              <input 
-              type="Range"
-              className="w-30 ml-2 accent-blue-600 mr-1"
-               />
-               <label className='font-medium text-orange-500 mr-2'>Length({length})</label>
+     
+        <div className='fixed mt-20 flex place-content-center inset-x-0 '>
+          <div className='w-[50%] h-auto bg-gray-700 px-3 py-2 rounded-lg '>
+            <h2 className='text-center text-2xl font-semibold mb-2 text-white'>Password Generator</h2>
+            <div className='flex'>
+              <input
+                type="text"
+                value={password}
+                placeholder='Password....'
+                readOnly
+                ref={passwordRef}
+                className='w-[85%] h-10 bg-gray-300 text-orange-500 font-medium rounded-l-lg px-6 text-lg ml-2 outline-none selection:text-orange-500 selection:bg-blue-300'
+              />
+              <button
+                onClick={copyFromClipboard}
+                className='bg-blue-700 px-4 h-10 rounded-r-lg text-lg text-white font-medium text-center focus:outline-2 focus:outline-orange-500 focus:bg-blue-800 hover:bg-blue-500 shrink-0'
+              >copy</button>
+            </div>
 
-               <input 
-               type="checkbox" 
-               className=' accent-blue-600'
-               />
-               <label className='font-medium text-orange-500 mx-2'>Numbers</label>
+            <div className='flex flex-wrap mt-5 mb-2 mx-1 '>
+              <input
+                type="range"
+                min={6}
+                max={100}
+                value={length}
+                onChange={(e) => { setLength(e.target.value) }}
+                className="w-30 ml-2 accent-blue-600 mr-1"
 
-               <input 
-               type="checkbox" 
-               className=' accent-blue-600'
-               />
-               <label className='font-medium text-orange-500 mx-2'>Characters</label>
+              />
+              <label className='font-medium text-orange-500 mr-2'>Length ({length})</label>
+
+              <input
+                type="checkbox"
+                id='numberInput'
+                defaultChecked={number}
+                className=' accent-blue-600'
+                onChange={() => { setNumber((prev) => !prev) }}
+              />
+              <label htmlFor="numberInput"
+                className='font-medium text-orange-500 mx-2'>Numbers</label>
+
+              <input
+                type="checkbox"
+                id='characterInput'
+                defaultChecked={character}
+                className=' accent-blue-600'
+                onChange={() => { setCharacter((prev) => !prev) }}
+              />
+              <label htmlFor="characterInput"
+                className='font-medium text-orange-500 mx-2'>Characters</label>
             </div>
 
           </div>
         </div>
-
-      </div>
     </>
   )
 }
