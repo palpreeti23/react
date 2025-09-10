981b455d6bc0dc4d1aa40acef14b2a66eca39c02
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2025-09-10 15:42:48 +0000

    fix: corrected typo in fetch response (.jason() → .json())

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -6,7 +6,7 @@ function useCurrencyInfo(currency) {
 
     useEffect(()=>{
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((res)=>res.jason())
+        .then((res)=>res.json())
         .then((res)=>setData(res[currency]))
         console.log(data)
     },[currency])
