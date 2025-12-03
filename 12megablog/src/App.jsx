a47f7831d4831ba623bcea1daba6204a02d11a2e
preeti pal <85545322+palpreeti23@users.noreply.github.com>
2025-12-03 17:17:37 +0000

    initial commit

---
+++ 12megablog/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ console.log(appwriteUrl)
+
+  return (
+    <>
+     <h2>
+      A BLOG WITH APPWRITE
+     </h2>
+    </>
+  )
+}
+
+export default App
