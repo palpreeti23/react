630c37d8f49452c5bea21bc4806f9f7288415e46
preeti pal <85545322+palpreeti23@users.noreply.github.com>
2025-12-04 16:44:25 +0000

    refactor: clean up index.js and improve file structure

--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -1,7 +1,16 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button";
+
 
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button
 }
