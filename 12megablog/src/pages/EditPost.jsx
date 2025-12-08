87e07a5e91512c6b3153dc839051f4f15b3b504f
preeti pal <85545322+palpreeti23@users.noreply.github.com>
2025-12-08 11:54:31 +0000

    done

---
+++ 12megablog/src/pages/EditPost.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParam } from "react-router-dom"
+import { Container, PostForm } from '../components'
+import appwriteService from "../appwrite/conf"
+
+
+export default function EditPost() {
+
+  const [posts, setPosts] = useState(null)
+  const {slug} = useParam();
+  const navigate = useNavigate()
+
+  useEffect(() => {
+
+    if (slug) {
+      appwriteService.getPost(posts).then((posts) => {
+        if (posts) {
+          setPosts(posts)
+        }
+      }
+
+      )
+    } else {
+      navigate('/')
+
+    }
+
+  }, [slug, navigate])
+  return posts ? (
+    <div className='py-8'>
+
+      <Container>
+        <PostForm posts={posts}/>
+      </Container>
+    </div>
+  ) : null
+
+
+
+}
